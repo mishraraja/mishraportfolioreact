7c627447e62d938b07cc67ee8f3d4cd0f8c403e4
Raja Mishra <121672623+mishraraja@users.noreply.github.com>
2023-09-15 22:20:45 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -11,7 +11,8 @@ function AboutCard() {
             Greetings everyone, I'm <span className="purple">Raja Mishra</span>,
             based in <span className="purple">Ranchi, India.</span>
             <br />
-            "I hold a Master's degree in Computer Applications (MCA) and boast a formidable track record with over two years of experience in software development. My specialization revolves around Java, where I wield expertise in Spring Framework, Hibernate, and Spring Boot.
+            "I hold a Master's degree in Computer Applications (MCA) and boast a formidable track record with over two years of experience in software development. 
+            My specialization revolves around Java, where I wield expertise in Spring Framework, Hibernate, and Spring Boot.
             I am driven by a relentless pursuit of excellence, thriving in the most intricate of technical landscapes. My true passion lies in the realm of microservices, where I architect solutions that not only meet but exceed expectations. If you seek a professional who excels in the art of precise and impactful software development, your quest ends here."</p>
           <p>Some of my hobbies include:</p>
           <ul>
